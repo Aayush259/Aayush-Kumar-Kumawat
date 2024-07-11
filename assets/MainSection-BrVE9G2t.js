4f31f0cb6faac485368aa534e15093a815522755
Aayush259 <Aayush259@users.noreply.github.com>
2024-07-11 15:24:29 +0000

    deploy: faa5bf55ea7cf73f3c7974a200c91f70445f47db

---
+++ assets/MainSection-BrVE9G2t.js
@@ -0,0 +1,1 @@
+import{j as t,O as a}from"./index-CAgq8AFC.js";/* empty css              */function s(){return t.jsx("main",{className:"main",children:t.jsx(a,{})})}export{s as default};
--- assets/MainSection-CEU8Z3yJ.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as a}from"./index-BGFS9Opg.js";/* empty css              */function s(){return t.jsx("main",{className:"main",children:t.jsx(a,{})})}export{s as default};
