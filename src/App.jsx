886318b321e1205ce4e46e297457102cb8432cbf
Square <aayushk4980@gmail.com>
2024-08-28 00:06:15 +0530

    Redirect to new portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
 import './styles/App.css';
 import Header from './components/Header.jsx';
 import Loader from './components/Loader.jsx';
@@ -6,12 +6,31 @@ const MainSection = lazy(() => import('./components/MainSection.jsx'));
 
 export default function App() {
 
+  // Redirect to new portfolio on page load.
+  useEffect(() => {
+    window.location.replace('https://aayush259.netlify.app/');
+  }, []);
+
   return (
     <>
-      <Header />
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          backgroundColor: 'transparent',
+        }}
+      >
+        <p>Redirecting to new Portfolio...</p>
+      </div>
+      {/* <Header />
       <Suspense fallback={<Loader />}>
         <MainSection />
-      </Suspense>
+      </Suspense> */}
     </>
   );
 };
