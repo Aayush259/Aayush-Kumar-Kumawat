7dce2311bb551ef4a1e10fcfabed7ee576d132de
Aayush259 <Aayush259@users.noreply.github.com>
2024-10-20 18:11:39 +0000

    deploy: 912243d303a7380dcaa3a84de24fbca7ea2eacec

--- assets/MainSection-Cj6sg7bI.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as a}from"./index-DCFfhnTL.js";/* empty css              */function s(){return t.jsx("main",{className:"main",children:t.jsx(a,{})})}export{s as default};
---
+++ assets/MainSection-DItuAwoU.js
@@ -0,0 +1,1 @@
+import{j as t,O as a}from"./index-Ccm7SDp9.js";/* empty css              */function s(){return t.jsx("main",{className:"main",children:t.jsx(a,{})})}export{s as default};
